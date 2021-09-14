7baaadba284d8fa6aac24f8f6346b82fb96c29ce
ugurb96 <ugur.bas096@gmail.com>
2021-09-14 11:11:39 +0300

    ReactFirst

---
+++ src/Components/Content.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import About from './About'
+import Contact from './Contact'
+import Menu from './Menu'
+function Content() {
+    return (
+        <>
+         <div className="w3-content" style={{ maxWidth: 1100 }}>
+            <About></About>
+            <Menu></Menu>
+            <Contact></Contact>
+        </div>
+        </>
+    )
+}
+
+export default Content
